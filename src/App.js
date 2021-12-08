af7ca71a3a5c0ae7ef027d87d5f614b5c8f5000f
jaeha <jaeha23@naver.com>
2021-12-08 20:12:41 +0900

    create Splashscreen.hide()

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import * as React from 'react';
+import React, { useEffect } from 'react';
+import SplashScreen from 'react-native-splash-screen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
 import LoginPage from './auth/LoginPage';
 import RegisterPage from './auth/RegisterPage';
 import MainPage from './tabs/Main.js';
 
 const Stack = createStackNavigator();
 function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="LOGIN">
