7be443c07eeace8c70b47e7811f88bf5a83b803b
jaeha <jaeha23@naver.com>
2021-12-15 16:04:32 +0900

    create LogoutButton

--- src/tabs/pages/home/DrawerContent.js
+++ src/tabs/pages/home/DrawerContent.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { DrawerContentScrollView, DrawerItem } from '@react-navigation/drawer';
+import styled from 'styled-components/native';
+import RNRestart from 'react-native-restart';
 
 import { storage } from '../../../config';
 
-const { getToken } = storage;
+const { getToken, removeToken } = storage;
+
+const StyledPressable = styled.Pressable`
+  align-self: center;
+  padding: 5px 10px;
+`;
+
+const StyledText = styled.Text`
+  font-size: 16px;
+  color: grey;
+  text-decoration: underline grey;
+`;
 
 function CustomDrawerContent(props) {
   const [cmdCode, setCmdCode] = useState('');
@@ -33,11 +45,29 @@ function CustomDrawerContent(props) {
     props.navigation.navigate('WEBVIEW', { url: web_url });
   }
 
+  async function onPressLogout() {
+    removeToken('isAutoLogin');
+    removeToken('token');
+    removeToken('id');
+    removeToken('pw');
+    removeToken('cmdCode');
+    removeToken('mainData');
+
+    const userData = await getToken('token');
+
+    if (userData == null) {
+      RNRestart.Restart();
+    }
+  }
+
   return (
     <DrawerContentScrollView {...props}>
       {menu.map((item, idx) => (
         <DrawerItem label={item.menu_name} onPress={() => goWebView(item.menu_url)} key={idx} />
       ))}
+      <StyledPressable onPress={onPressLogout}>
+        <StyledText>로그아웃</StyledText>
+      </StyledPressable>
     </DrawerContentScrollView>
   );
 }
