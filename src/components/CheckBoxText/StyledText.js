e7c11597a4b58ba1103f1b23a91cf6926641de46
jaeha <jaeha23@naver.com>
2021-12-20 22:16:51 +0900

    plus margin-left

--- src/components/CheckBoxText/StyledText.js
+++ src/components/CheckBoxText/StyledText.js
@@ -3,7 +3,7 @@ import styled from 'styled-components/native';
 const StyledText = styled.Text`
   font-size: 14px;
   color: rgba(0, 0, 0, 0.38);
-  margin-left: 12px;
+  margin-left: 20px;
 `;
 
 export default StyledText;
